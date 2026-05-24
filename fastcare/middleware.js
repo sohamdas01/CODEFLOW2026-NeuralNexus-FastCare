6d471db59a938912585c47e44fa95e5e8be3d248
TheArcaneVamp <rishabhchakrabortyofficial@gmail.com>
2026-05-24 18:12:48 +0530

    updating

--- fastcare/middleware.js
+++ fastcare/middleware.js
@@ -1,5 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
+// Add this line to force Node.js runtime
+export const runtime = 'nodejs';
+
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)','/']);
 
 export default clerkMiddleware(async (auth, req) => {
@@ -10,7 +13,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files, unless founsd in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
